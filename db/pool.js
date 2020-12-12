9acb225d618f5c63bdb65a1bd1323038b2bc7246
Benjie Kibblewhite <benjie@benjie.ca>
2020-12-12 15:42:03 -0500

    remove ssl

--- db/pool.js
+++ db/pool.js
@@ -4,7 +4,7 @@ const Pool = require("pg").Pool;
 // dotenv.config();
 console.log(process.env.DATABASE_URL);
 const pool = new Pool({
-  connectionString: `${process.env.DATABASE_URL}?sslmode=require`,
+  connectionString: `${process.env.DATABASE_URL}`,
 });
 
 module.exports = { pool };
