962b190393e522e893e1edd16896fc93a2a49395
Benjie Kibblewhite <benjie@benjie.ca>
2020-12-12 18:03:31 -0500

    cleanup pool

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,8 +1,5 @@
 const Pool = require("pg").Pool;
-// var dotenv = require("dotenv");
 
-// dotenv.config();
-console.log(process.env.DATABASE_URL);
 const pool = new Pool({
   connectionString: `${process.env.DATABASE_URL}`,
 });
