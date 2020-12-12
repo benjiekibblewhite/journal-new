e13dae8e41a56460f433bc8c4236fbb392eeb372
Benjie Kibblewhite <benjie@benjie.ca>
2020-12-12 14:23:07 -0500

    change port name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 var app = express();
 app.use(bodyParser.json());
