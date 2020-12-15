37b40540ffdbb0e1e0857148a2eb58d93cb4af9f
Benjie Kibblewhite <benjie@benjie.ca>
2020-12-14 19:51:16 -0500

    make users table first, then posts

--- src/db/setup.js
+++ src/db/setup.js
@@ -3,13 +3,12 @@ const { pool } = require("./pool");
 async function initDatabse() {
   await pool
     .query(
-      "CREATE TABLE posts (ID SERIAL PRIMARY KEY, created_at TIMESTAMP, updated_at TIMESTAMP, title TEXT, body TEXT, FOREIGN KEY (userID) REFERENCES USERS (userID))"
+      "CREATE TABLE users (userID SERIAL PRIMARY KEY, registered_At TIMESTAMP, name TEXT, email TEXT UNIQUE, password TEXT)"
     )
     .catch(console.error);
-
   await pool
     .query(
-      "CREATE TABLE users (userID SERIAL PRIMARY KEY, registered_At TIMESTAMP, name TEXT, email TEXT UNIQUE, password TEXT)"
+      "CREATE TABLE posts (ID SERIAL PRIMARY KEY, created_at TIMESTAMP, updated_at TIMESTAMP, title TEXT, body TEXT, FOREIGN KEY (userID) REFERENCES USERS (userID))"
     )
     .catch(console.error);
 }
@@ -31,7 +30,6 @@ async function addUsers() {
 
 async function init() {
   await initDatabse();
-  await addUsers();
 }
 
 init();
